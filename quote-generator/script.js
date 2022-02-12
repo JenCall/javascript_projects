4f9fc34282be795b12b39e762e0f01ec50bc9f24
Jeniffer Callou <jenifferprc@gmail.com>
2022-02-12 16:10:21 +0100

    adjust js

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,14 +1,25 @@
-// Get Quotes From API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('quote-author');
+const twitterBtn = document.getElementById('quote-container');
+const newQuoteBtn = document.getElementById('quote-container');
 
 let apiQuotes = []
+
+// show new quote
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    console.log(quote)
+}
 
 // Asychronous catch request
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
     try {
-        const response = await fetch(apiUrl) //only setting the response when we actually have data
+        const response = await fetch(apiUrl) // only setting the response when we actually have data
         apiQuotes = await response.json();
-        console.log(apiQuotes[12]);
+        newQuote();
     } catch (error) {
         // Catch Error Here
     }
